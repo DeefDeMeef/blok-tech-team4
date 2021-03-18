8a275b1150583487d7f2f8e4a76af230d442c284
KaivWezel <kaivanwezel@gmail.com>
2021-03-18 13:17:41 +0100

    added necessary packages

---
+++ controllers/profileController.js
@@ -0,0 +1,1 @@
+// profileCreate, profile
--- index.js
+++ index.js
@@ -2,14 +2,20 @@ require("dotenv/config");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const port = 3000;
+const port = 5000;
 const path = require("path");
 const uri = process.env.DB_HOST;
 app.use(express.static("static"));
 
+app.set('view engine', 'ejs');
+
 app.listen(port, () => {
   console.log(`listening at port ${port}`);
 });
+
+app.get('/', (req,res)=>{
+  res.send('hello')
+})
 
 mongoose
   .connect(uri, {
