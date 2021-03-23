05dc32bb86e1bc6620ce814c4f0ebbdaf31f8293
KaivWezel <kaivanwezel@gmail.com>
2021-03-23 10:58:56 +0100

    type:module verwijderd

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,4 +1,5 @@
 const express = require(`express`);
+// eslint-disable-next-line new-cap
 const router = express.Router();
 const profileController = require(`../controllers/profileController`);
 const upload = require(`../controllers/util/upload`);
