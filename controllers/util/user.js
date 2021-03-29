2c2371f391bdd42a14b61ea16ab6b4595a4355bc
DeefDeMeef <daveyzaal@hotmail.nl>
2021-03-29 12:13:31 +0200

    chat bug gefixt waar eeen nieuwe insstantie van socket werd aangemaakt

---
+++ controllers/util/user.js
@@ -0,0 +1,25 @@
+const users = []
+
+  function userJoin(id, username, room) {
+    const user = { id, username, room };
+    users.push(user)
+    console.log(users)
+    return user
+  }
+
+  function getCurrentUser(id) {
+    return users.find(user => user.id === id)
+  }
+
+  function userLeave(id) {
+    const index = users.findIndex(user => user.id === id)
+    if (index !== -1) {
+      return users.splice(index, 1)[0]
+    }
+  }
+
+module.exports = {
+    userJoin,
+    getCurrentUser,
+    userLeave
+}
